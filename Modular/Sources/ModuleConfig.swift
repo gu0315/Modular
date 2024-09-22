1ee50576a874e8a34661dc3329deb96b98b89166
gqx <39515706+gu0315@users.noreply.github.com>
2024-09-22 15:00:00 +0800

    add

--- Modular/Sources/ModuleConfig.swift
+++ Modular/Sources/ModuleConfig.swift
@@ -9,15 +9,11 @@ import UIKit
 
 class ModuleConfig: NSObject {
     
-    private var default404ModuleURL: String?
+    public var default404ModuleURL: String?
     
     public static let share = ModuleConfig()
     
     private override init() {
         super.init()
-    }
-    
-    public func override404ModuleURL(_ url: String) {
-        default404ModuleURL = url
     }
 }
